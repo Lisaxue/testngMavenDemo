10f21294bb9e1504390947b06d8f9d1bff26d08d
chenxuejiao <chenxuejiao@le.com>
2017-06-01 17:48:44 +0800

    first commit

---
+++ testngMaven/src/test/java/com/javacodegeeks/testng/maven/testngMaven/TestNgMavenExampleTest.java
@@ -0,0 +1,11 @@
+package com.javacodegeeks.testng.maven.testngMaven;
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenExampleTest{
+	@Test
+	public void exampleOfTestNgMaven() {
+	        System.out.println("This is TestNG-Maven Example");
+	    }
+
+}
---
+++ testngMaven/src/test/java/com/javacodegeeks/testng/maven/testngMaven/TestNgMavenSecond.java
@@ -0,0 +1,11 @@
+package com.javacodegeeks.testng.maven.testngMaven;
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenSecond {
+
+	@Test
+	public void oneMoreTest() {
+		System.out.println("This is TestNG-Second Example");
+	}
+}
